6a2e6b11057a252df65df47a1836c6b05e8b54ea
Naveenravi07 <gamerkidnav@gmail.com>
2023-09-11 23:42:48 +0530

    Performance Improvement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use audio_scrapper::{Config, InputMethods};
+use audio_scrapper::InputMethods;
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use rspotify::{
     model::PlaylistId,
     prelude::{BaseClient, OAuthClient},
     scopes, AuthCodeSpotify, Credentials, OAuth,
 };
-use std::{fs, process, rc::Rc, sync::Arc, thread};
+use std::{fs, process, sync::Arc, thread};
 use youtube_dl::{SearchOptions, YoutubeDl};
+use std::time:: Instant;
+
 
 pub struct PlaylistTracks {
     tracks: Vec<String>,
@@ -21,14 +24,14 @@ async fn main() {
             eprintln!("Problems in parsing arguments : {}", message);
             process::exit(1);
         }),
-    );
+        );
 
     println!("{:?}", config);
 
     match config.method {
         InputMethods::File => {
-            let content = fs::read_to_string(&config.input_file.as_ref().unwrap())
-                .unwrap_or_else(|_err| {
+            let content =
+                fs::read_to_string(&config.input_file.as_ref().unwrap()).unwrap_or_else(|_err| {
                     eprintln!("No file found at specified location");
                     process::exit(1);
                 });
@@ -50,59 +53,63 @@ async fn main() {
             }
         }
 
-        InputMethods::Spotify => {}
+        InputMethods::Spotify => {
+            let start = Instant::now();
+
+            let credentials = Credentials::new(
+                "7d4cca88e358409488db59c8dea2d3f9",
+                "e63d6a668a5d43a08c095d8cc8d7b6cb",
+                );
+
+            let oauth = OAuth {
+                redirect_uri: "http://localhost:42069".to_string(),
+                scopes: scopes!("user-read-private user-read-email"),
+                ..Default::default()
+            };
+
+            let spotify = AuthCodeSpotify::new(credentials, oauth);
+            let url = spotify.get_authorize_url(false).unwrap();
+            spotify.prompt_for_token(&url).await.unwrap();
+
+            println!("Fetching your playlists ");
+            let playlist = spotify
+                .current_user_playlists_manual(None, None)
+                .await
+                .unwrap();
+
+            for (index, track) in playlist.items.iter().enumerate() {
+                println!("Press {} to download {}", index, track.name);
+            }
+
+            let mut inp_string = String::new();
+            std::io::stdin()
+                .read_line(&mut inp_string)
+                .expect("Failed to read input");
+            let num: usize = inp_string.trim().parse().unwrap();
+
+            let playlisturl = playlist.items[num].id.to_string();
+
+            println!("Downloading {}", playlist.items[num].name);
+
+            let mut offset: u32 = 0;
+
+            while {
+                let results = fetch_tracks_of_playlist(&spotify, &playlisturl, Some(offset)).await;
+                offset += 100;
+                downlaod_tracks_from_youtube(&results.tracks, &config.output_dir);
+
+                results.tracks.len() < usize::try_from(results.total.unwrap()).unwrap()
+            } {}
+            let duration = start.elapsed();
+            println!("Time elapsed in expensive_function() is: {:?}", duration);
+        }
     }
-
-    let credentials = Credentials::new(
-        "7d4cca88e358409488db59c8dea2d3f9",
-        "e63d6a668a5d43a08c095d8cc8d7b6cb",
-    );
-
-    let oauth = OAuth {
-        redirect_uri: "http://localhost:42069".to_string(),
-        scopes: scopes!("user-read-private user-read-email"),
-        ..Default::default()
-    };
-
-    let spotify = AuthCodeSpotify::new(credentials, oauth);
-    let url = spotify.get_authorize_url(false).unwrap();
-    spotify.prompt_for_token(&url).await.unwrap();
-
-    println!("Fetching your playlists ");
-    let playlist = spotify
-        .current_user_playlists_manual(None, None)
-        .await
-        .unwrap();
-
-    for (index, track) in playlist.items.iter().enumerate() {
-        println!("Press {} to download {}", index, track.name);
-    }
-
-    let mut inp_string = String::new();
-    std::io::stdin()
-        .read_line(&mut inp_string)
-        .expect("Failed to read input");
-    let num: usize = inp_string.trim().parse().unwrap();
-
-    let playlisturl = playlist.items[num].id.to_string();
-
-    println!("Downloading {}", playlist.items[num].name);
-
-    let mut offset: u32 = 0;
-
-    while {
-        let results = fetch_tracks_of_playlist(&spotify, &playlisturl, Some(offset)).await;
-        offset += 100;
-        downlaod_tracks_from_youtube(&results.tracks, &config.output_dir);
-
-        results.tracks.len() < usize::try_from(results.total.unwrap()).unwrap()
-    } {}
 }
 async fn fetch_tracks_of_playlist(
     spotify: &AuthCodeSpotify,
     playlisturl: &String,
     offset: Option<u32>,
-) -> PlaylistTracks {
+    ) -> PlaylistTracks {
     let mut results: Vec<String> = Vec::new();
     let songs = spotify
         .playlist_items_manual(
@@ -111,7 +118,7 @@ async fn fetch_tracks_of_playlist(
             None,
             Some(100),
             offset,
-        )
+            )
         .await
         .unwrap();
 
@@ -132,20 +139,17 @@ async fn fetch_tracks_of_playlist(
 }
 
 fn downlaod_tracks_from_youtube(tracks: &Vec<String>, output_dir: &String) {
-    for music in tracks {
-        let music = music.clone();
-        let output_dir = output_dir.clone();
-        let threads = thread::spawn(move || {
-            let options = SearchOptions::youtube(&music);
-            let audio = YoutubeDl::search_for(&options)
-                .extract_audio(true)
-                .output_template(&music)
-                .download_to(output_dir);
-            match audio {
-                Ok(_) => println!("{} Download Successfull", music),
-                Err(_) => println!("Err Downloading {} from youtube", music),
-            }
-        });
-        threads.join().unwrap();
-    }
+    tracks.par_iter().for_each(|music|{
+        let options = SearchOptions::youtube(music);
+        let audio = YoutubeDl::search_for(&options)
+            .extract_audio(true)
+            .output_template(music)
+            .download_to(output_dir);
+        match audio {
+            Ok(_) => println!("{} Download Successfull", music),
+            Err(_) => println!("Err Downloading {} from youtube", music),
+        }
+
+    })        
 }
+
